f51b9d69e02ecbc1e80aa3f62142eabff6724a9a
Jordan Byrne <jordan.byrne25@thehutgroup.com>
2024-02-16 11:07:18 +0000

    user input

--- messaging/server.go
+++ messaging/server.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	// "github.com/gorilla/websocket"
+)
+
+var url string = "http://localhost:8000"
+
 
 func main() {
-    fmt.Println("Hello, World!")
+		message := get_input()
+    log.Println("server:", url)
+		log.Println(message)
+}
+
+func get_input() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Message:")
+	message, _ := reader.ReadString('\n')
+	return message
 }
